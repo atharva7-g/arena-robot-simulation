34faf72a22ff795a734e5a0d3bd5333de9450293
Shashank Madhusudan <mad.shadank@gmail.com>
2025-09-25 02:13:00 +0530

    add chassis.stl

--- arena-robot-simulation/mec_mobile_gazebo/launch/spawn_robot.launch.py
+++ arena-robot-simulation/mec_mobile_gazebo/launch/spawn_robot.launch.py
@@ -8,65 +8,68 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Comm
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description():
 
-    pkg_urdf_path = get_package_share_directory('mec_mobile_description')
-    pkg_gazebo_path = get_package_share_directory('mec_mobile_gazebo')
+def generate_launch_description():
+    pkg_urdf_path = get_package_share_directory("mec_mobile_description")
+    pkg_gazebo_path = get_package_share_directory("mec_mobile_gazebo")
 
     gazebo_models_path, ignore_last_dir = os.path.split(pkg_urdf_path)
-    #os.environ["GZ_SIM_RESOURCE_PATH"] += os.pathsep + gazebo_models_path
+    # os.environ["GZ_SIM_RESOURCE_PATH"] += os.pathsep + gazebo_models_path
 
     rviz_launch_arg = DeclareLaunchArgument(
-        'rviz', default_value='true',
-        description='Open RViz.'
+        "rviz", default_value="true", description="Open RViz."
     )
 
     world_arg = DeclareLaunchArgument(
-        'world', default_value='empty.sdf',
-        description='Name of the Gazebo world file to load'
+        "world",
+        default_value="empty.sdf",
+        description="Name of the Gazebo world file to load",
     )
 
     model_arg = DeclareLaunchArgument(
-        'model', default_value='robot_3d.urdf.xacro',
-        description='Name of the URDF description to load'
+        "model",
+        default_value="robot_3d.urdf.xacro",
+        description="Name of the URDF description to load",
     )
 
     sarm_arg = DeclareLaunchArgument(
-        'sarm', default_value='sarm.urdf.xacro',
-        description='Name of the URDF description to load'
+        "sarm",
+        default_value="sarm.urdf.xacro",
+        description="Name of the URDF description to load",
     )
 
     # Define the path to your URDF or Xacro file
-    urdf_file_path = PathJoinSubstitution([
-        pkg_urdf_path,  # Replace with your package name
-        "urdf","robots",
-        LaunchConfiguration('model')  # Replace with your URDF or Xacro file
-    ])
+    urdf_file_path = PathJoinSubstitution(
+        [
+            pkg_urdf_path,  # Replace with your package name
+            "urdf",
+            "robots",
+            LaunchConfiguration("model"),  # Replace with your URDF or Xacro file
+        ]
+    )
 
-    sarm_file_path = PathJoinSubstitution([
-        pkg_urdf_path,
-        "urdf","sarm",
-        LaunchConfiguration('sarm')
-    ])
+    sarm_file_path = PathJoinSubstitution(
+        [pkg_urdf_path, "urdf", "sarm", LaunchConfiguration("sarm")]
+    )
 
     world_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_path, 'launch', 'world.launch.py'),
+            os.path.join(pkg_gazebo_path, "launch", "world.launch.py"),
         ),
         launch_arguments={
-        'world': LaunchConfiguration('world'),
-        }.items()
+            "world": LaunchConfiguration("world"),
+        }.items(),
     )
 
     # Launch rviz
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', os.path.join(pkg_urdf_path, 'rviz', 'rviz.rviz')],
-        condition=IfCondition(LaunchConfiguration('rviz')),
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(pkg_urdf_path, "rviz", "rviz.rviz")],
+        condition=IfCondition(LaunchConfiguration("rviz")),
         parameters=[
-            {'use_sim_time': True},
-        ]
+            {"use_sim_time": True},
+        ],
     )
 
     # Spawn the URDF model using the `/world/<world_name>/create` service
@@ -74,68 +77,87 @@ def generate_launch_description():
         package="ros_gz_sim",
         executable="create",
         arguments=[
-            "-name", "lfr",
-            "-topic", "robot_description",
-            "-x", "-1.94", "-y", "0.78", "-z", "0.2", "-Y", "-1.6"  # Initial spawn position
+            "-name",
+            "lfr",
+            "-topic",
+            "robot_description",
+            "-x",
+            "-1.94",
+            "-y",
+            "0.78",
+            "-z",
+            "0.2",
+            "-Y",
+            "-1.6",  # Initial spawn position
         ],
-
         output="screen",
         parameters=[
-            {'use_sim_time': True},
-        ]
+            {"use_sim_time": True},
+        ],
     )
 
     spawn_sarm_node = Node(
         package="ros_gz_sim",
         executable="create",
         arguments=[
-            "-name", "sarm",
-            "-topic", "/sarm_description",
-            "-x", "1.0", "-y", "0.0", "-z", "0.2",   # Initial position
-            "-R", "+1.5708",                         # Roll = -π/2
-            "-P", "0.0",                             # Pitch = 0
-            "-Y", "0.0"                              # Yaw = 0
+            "-name",
+            "sarm",
+            "-topic",
+            "/sarm_description",
+            "-x",
+            "1.0",
+            "-y",
+            "0.0",
+            "-z",
+            "0.2",  # Initial position
+            "-R",
+            "-1.5708",  # Roll = -π/2
+            "-P",
+            "0.0",  # Pitch = 0
+            "-Y",
+            "0.0",  # Yaw = 0
         ],
         output="screen",
         parameters=[
-            {'use_sim_time': True},
-        ]
+            {"use_sim_time": True},
+        ],
     )
 
-
-
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
         parameters=[
-            {'robot_description': Command(['xacro', ' ', urdf_file_path]),
-             'use_sim_time': True},
+            {
+                "robot_description": Command(["xacro", " ", urdf_file_path]),
+                "use_sim_time": True,
+            },
         ],
-        remappings=[
-            ('/tf', 'tf'),
-            ('/tf_static', 'tf_static')
-        ]
+        remappings=[("/tf", "tf"), ("/tf_static", "tf_static")],
     )
 
     sarm_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='sarm_state_publisher',
-        output='screen',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="sarm_state_publisher",
+        output="screen",
         parameters=[
-            {'robot_description': Command(['xacro ', sarm_file_path]),
-             'use_sim_time': True},
+            {
+                "robot_description": Command(["xacro ", sarm_file_path]),
+                "use_sim_time": True,
+            },
         ],
         remappings=[
-            ('robot_description', 'sarm_description'),  # Remap to sarm_description topic
+            (
+                "robot_description",
+                "sarm_description",
+            ),  # Remap to sarm_description topic
             # Uncomment these if you want separate TF trees:
             # ('/tf', 'sarm/tf'),
             # ('/tf_static', 'sarm/tf_static')
-        ]
+        ],
     )
-
 
     # Node to bridge messages like /cmd_vel and /odom
     gz_bridge_node = Node(
@@ -157,8 +179,8 @@ def generate_launch_description():
         ],
         output="screen",
         parameters=[
-            {'use_sim_time': True},
-        ]
+            {"use_sim_time": True},
+        ],
     )
 
     # joint_state_publisher_gui_node = Node(
@@ -179,6 +201,5 @@ def generate_launch_description():
     launchDescriptionObject.add_action(gz_bridge_node)
     launchDescriptionObject.add_action(spawn_sarm_node)
     launchDescriptionObject.add_action(sarm_state_publisher_node)
-
 
     return launchDescriptionObject
