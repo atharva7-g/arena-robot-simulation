568a5b7023c0ed7073cee6e4c7fc4c6fb66eafc7
Shashank Madhusudan <mad.shadank@gmail.com>
2025-09-26 20:37:30 +0530

    TODO: fix xacro

--- sarm_base_control/setup.py
+++ sarm_base_control/setup.py
@@ -1,30 +1,29 @@
 from setuptools import find_packages, setup
 
-package_name = 'sarm_base_control'
+package_name = "sarm_base_control"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='hephan',
-    maintainer_email='hephannn@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="ash-blake",
+    maintainer_email="hephannn@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'move_base_to_position = sarm_base_control.move_base_to_position:main'
+        "console_scripts": [
+            "move_base_to_position = sarm_base_control.move_base_to_position:main"
         ],
     },
 )
